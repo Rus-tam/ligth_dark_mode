5394f5658a3156edd41db361798607386e3f2a7f
Rustam <ahmetov.rustam2011@gmail.com>
2020-12-21 18:15:03 +0500

    last_commit

--- src/index.js
+++ src/index.js
@@ -6,39 +6,40 @@ const image2 = document.getElementById('image2')
 const image3 = document.getElementById('image3')
 const textBox = document.getElementById('text-box')
 
-//Dark Mode Styles
-const darkMode = () => {
-    nav.style.backgroundColor = 'rgb(0 0 0 / 50%)'
-    textBox.style.backgroundColor = 'rgb(255 255 255 / 50%)'
-    toggleIcon.children[0].textContent = 'Dark Mode'
-    toggleIcon.children[1].classList.replace('fa-sun', 'fa-moon')
-    image1.src = './img/undraw_proud_coder_dark.svg'
-    image2.src = './img/undraw_feeling_proud_dark.svg'
-    image3.src = './img/undraw_conceptual_idea_dark.svg'
-}
-
-//Light Mode Styles
-const lightMode = () => {
-    nav.style.backgroundColor = 'rgb(255 255 255 / 50%)'
-    textBox.style.backgroundColor = 'rgb(0 0 0 / 50%)'
-    toggleIcon.children[0].textContent = 'Light Mode'
-    toggleIcon.children[1].classList.remove('fa-moon')
-    toggleIcon.children[1].classList.add('fa-sun')
-    image1.src = './img/undraw_proud_coder_light.svg'
-    image2.src = './img/undraw_feeling_proud_light.svg'
-    image3.src = './img/undraw_conceptual_idea_light.svg'
+//Dark Light Mode Styles
+const toggleDarkLightMode = (isDark) => {
+    nav.style.backgroundColor = isDark ? 'rgb(0 0 0 / 50%)' : 'rgb(255 255 255 / 50%)'
+    textBox.style.backgroundColor = isDark ? 'rgb(255 255 255 / 50%)' : 'rgb(0 0 0 / 50%)'
+    toggleIcon.children[0].textContent = isDark ? 'Dark Mode' : 'Light Mode'
+    isDark ? toggleIcon.children[1].classList.replace('fa-sun', 'fa-moon') : toggleIcon.children[1].classList.replace('fa-moon', 'fa-sun')
+    isDark ? image1.src = `./img/undraw_proud_coder_${dark}.svg` : image1.src = `./img/undraw_proud_coder_${light}.svg`
+    isDark ? image2.src = `./img/undraw_feeling_proud_${dark}.svg` : image2.src = `./img/undraw_feeling_proud_${light}.svg`
+    isDark ? image3.src = `./img/undraw_conceptual_idea_${dark}.svg` : image3.src = `./img/undraw_conceptual_idea_${light}.svg`
 }
 
 //Switch Theme Dynamically
 const switchTheme = (e) => {
     if (e.target.checked) {
         document.documentElement.setAttribute('data-theme', 'dark')
-        darkMode()
+        localStorage.setItem('theme', 'dark')
+        toggleDarkLightMode(true)
     } else {
         document.documentElement.setAttribute('data-theme', 'light')
-        lightMode()
+        localStorage.setItem('theme', 'light')
+        toggleDarkLightMode(false)
     }
 }
 
 //Event Listner
 toggleSwitch.addEventListener('change', switchTheme)
+
+//Check Local Storage For Theme
+const currentTheme = localStorage.getItem('theme')
+if (currentTheme) {
+    document.documentElement.setAttribute('data-theme', currentTheme)
+
+    if (currentTheme === 'dark') {
+        toggleSwitch.checked = true
+        toggleDarkLightMode(true)
+    }
+}
